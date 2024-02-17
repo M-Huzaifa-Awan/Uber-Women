840501b247eb357a60ad6d870153bfc2cf029c3d
Muhammad Abdullah Niazi <abdullahniaziuni@gmail.com>
2024-02-17 22:41:05 +0500

    added spinner and made comingsoon.html better

---
+++ Controller/dashboard.js
@@ -0,0 +1,9 @@
+function asyncing(){
+    let a = document.getElementById("if");
+    a.setAttribute('async', '');
+}
+function showspin(){
+let a = document.getElementById('spiner');
+a.classList.add('visually-hidden');
+}
+asyncing();
--- Controller/login.js
+++ Controller/login.js
@@ -1,14 +1,18 @@
+
 function check(){
     let number = document.getElementById('number').value;
     number = +number;
     let password = document.getElementById('Password').value;
-    let saveddata = {number:03078222203,password:"zaifa"};
-let isUser = false;
+    let saveddata = {number:3078222203,password:"zaifa"};
+    let isUser = false;
     if (number===saveddata.number && password === saveddata.password) {
-        isUser = true;
         location.replace("./dashboard.html");
+        isUser = true;
+        document.getElementById('de').checked = true;
+        return true;
     }
     else{
         alert(isUser);
+        return false;
     }
 }
